91e67523980b7e2f50d81039b66cc4425383fa52
leodeslf <leodeslf@gmail.com>
2024-04-12 22:46:59 -0300

    Remove hero.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-// import Hero from "./components/Hero/Hero";
 import Intro from "./components/Intro";
 import PersonalInformation from "./components/PersonalInformation";
 import Projects from "./components/Projects/Projects";
 
 export default function App() {
   return (<>
-    {/* <Hero /> */}
-    <h1>
-      <a
-        href="/"
-        title="Leonardo de Souza Leal Figueira"
-      >
-        Leonardo de S. Leal F.
-      </a>
-    </h1>
     <main>
       <Intro />
       <Projects />

bea39d05b579a9dfa1befbe78c319e5c22d8250c
leodeslf <leodeslf@gmail.com>
2024-04-13 23:07:03 -0300

    Add/bring h1 to intro. Update copy.

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,11 +1,14 @@
 export default function Intro() {
   return (
     <section className="intro">
+      <h1 title="Leonardo de Souza Leal Figueira">
+        Leonardo de S. Leal F.
+      </h1>
       <h2>
         Frontend Developer
       </h2>
       <p>
-        &ldquo;<strong>Aprendiz</strong> a tiempo completo; <strong>Optimista</strong> con mis objetivos;<br /> <strong>Motivado</strong> por retos difíciles; <strong>Perfeccionista</strong> en lo que hago;<br /> Mi anhelo es crecer sin límites, transformar mi curiosidad e<br /> ingenio en una fuente de valor constante y así aportar más,<br /> <strong>mucho más que tecnología</strong>.&rdquo;
+        <strong>Aprendiz</strong> a tiempo completo; <strong>Optimista</strong> con mis objetivos;<br /> <strong>Motivado</strong> por retos difíciles; <strong>Perfeccionista</strong> en lo que hago;<br /> Mi anhelo es mejorar y convertir mi curiosidad e ingenio en<br /> <strong>mucho más que tecnología</strong>.
       </p>
     </section>
   );

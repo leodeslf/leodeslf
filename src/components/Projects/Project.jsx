1c07355197810d49aca26079140f0173f3ca7f0d
leodeslf <leodeslf@gmail.com>
2024-04-13 23:08:05 -0300

    Refactor component.

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,47 +1,50 @@
 import { Fragment } from "react";
+import { capitalize } from ".";
 
 export default function Project({
   about,
   argument,
-  // id,
   links,
   title,
-  // tools,
+  tools,
   Preview
 }) {
   return (
     <article className="project">
-      <span className="project__description">
+      <span className="project__preview">
+        <Preview />
+      </span>
+      <span className="project__summary">
         <div className="project__header">
           <h3>
             <a
               href={links[0].url}
-              title={links[0].name}
+              title={`${capitalize(links[0].name)}.`}
             >
               {title}
             </a>
           </h3> / <h4>{about}</h4>
         </div>
-        <p>
-          Argumento: {argument}.
-          Links: {links.map((link, i) =>
+        <p className="project__description">
+          Motivación: {argument}. Ver {links.map((link, i) =>
             <Fragment key={i}>
-              {i > 0 ? ', ' : ''}<a
+              {i > 0 ? (i === links.length - 1 ? ' y ' : ', ') : ''}<a
                 href={link.url}
               >
                 {link.name}
               </a>
             </Fragment>
           )}.
-          {/* Nuevo: {tools.new.map((newTool, i) => <>
-            {i > 0 && ', '}<strong key={i}>
-              {newTool}
-            </strong>
-          </>)}. */}
         </p>
-      </span>
-      <span className="project__preview">
-        <Preview />
+        <p></p>
+        <p className="project__new-stack">
+          Aprendizaje relevante: {tools.new.map((newTool, i) =>
+            <Fragment key={i}>
+              {i > 0 && ', '}<strong>
+                {newTool}
+              </strong>
+            </Fragment>)}.
+        </p>
       </span>
     </article>
   );

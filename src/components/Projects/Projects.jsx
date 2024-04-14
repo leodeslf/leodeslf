912462fd5c80833e28d30ccaba2983cbcc724fdd
leodeslf <leodeslf@gmail.com>
2024-04-13 23:22:14 -0300

    Refactor/simplify component.

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,4 +1,3 @@
-import Casi from './Casi/Casi';
 import Fontima from './Fontima/Fontima';
 import Ephy from './Ephy/Ephy';
 import Skeleton from './Skeleton/Skeleton';
@@ -6,7 +5,6 @@ import Project from './Project';
 import projects from '../../json/projects.json';
 import Tfp from './Tfp/Tfp';
 import Tw from './Tw/Tw';
-import Tw2 from './Tw2/Tw2';
 import Vec from './Vec/Vec';
 
 // Rollup doesn't support lazy imports with dynamic paths. Hardcode is the way:
@@ -14,10 +12,7 @@ const previews = [
   Vec,
   Tfp,
   Tw,
-  // Tfp2,
-  Casi,
   Skeleton,
-  Tw2,
   Ephy,
   Fontima
 ];
@@ -26,16 +21,14 @@ export default function Projects() {
   return (
     <section className="projects">
       <h2>Experimentos</h2>
-      <div className="project__list">
-        {projects.map((project, i) =>
-          <Project {...{
-            ...project,
-            Preview: previews[i]
-          }}
-            key={project.id}
-          />
-        )}
-      </div>
-    </section >
+      {projects.map((project, i) =>
+        <Project {...{
+          ...project,
+          Preview: previews[i]
+        }}
+          key={project.id}
+        />
+      )}
+    </section>
   );
 }
